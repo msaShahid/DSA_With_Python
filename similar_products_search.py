41b35dbcf95e60198d38b3feef02a9efdac7240f
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-06 23:24:32 +0530

    Add functionality to find similar products from the product list
    - Implement a function to compare products by their first character
    - Handle case-insensitivity for comparisons
    - Update output formatting to display results line-by-line

---
+++ similar_products_search.py
@@ -0,0 +1,16 @@
+from products_list import products
+
+
+def similar_product_search(products):
+    similar_product = []
+    for p1 in range(len(products)):
+        for p2 in range(p1 + 1, len(products)):
+            if products[p1][0].lower() == products[p2][0].lower():
+                similar_product.append((products[p1], products[p2]))
+    return similar_product
+
+final_result = similar_product_search(products)
+
+print("-- Similar Products list --")
+for pair in final_result:
+    print(pair)
