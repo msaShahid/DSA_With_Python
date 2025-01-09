1fad0a95f2682da9b59ae8195e6216bfb722f15c
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-09 22:15:12 +0530

    `Updated products list from string to dictionary format and modified similar products search function accordingly.`

--- similar_products_search.py
+++ similar_products_search.py
@@ -5,8 +5,8 @@ def similar_product_search(products):
     similar_product = []
     for p1 in range(len(products)):
         for p2 in range(p1 + 1, len(products)):
-            if products[p1][0].lower() == products[p2][0].lower():
-                similar_product.append((products[p1], products[p2]))
+            if products[p1]['name'][0].lower() == products[p2]['name'][0].lower():
+                similar_product.append((products[p1]['name'], products[p2]['name']))
     return similar_product
 
 final_result = similar_product_search(products)
